7322e40ae05877acf70187b647561ec153d03ca7
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-08-26 17:55:38 -0600

    start

---
+++ components/Navbar.tsx
@@ -0,0 +1,368 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div className="flex flex-col bg-neutral-300 items-center justify-center py-8">
+			<div className="flex text-neutral-800 font-light gap-8 text-[0.9rem]">
+				<a href="">Some Link</a>
+				<a href="">Some Link</a>
+				<a href="">Some Link</a>
+			</div>
+			<h1 className="text-[4rem] text-neutral-950">NEW YORK TIMES</h1>
+			<div className="flex text-neutral-950 gap-8 text-[0.9rem]">
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<a href="" className="hover:text-neutral-600">
+						Some Link
+					</a>
+					<div className="absolute hidden group-hover:flex bg-neutral-100 p-4 mt-0 rounded shadow-lg min-w-[20vw] left-[50%] translate-x-[-50%] items-center gap-12">
+						<div className="flex flex-col gap-4">
+							<h2>History</h2>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+						<div className="flex flex-col gap-4">
+							<h2>Politics</h2>
+
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 1
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 2
+							</a>
+							<a
+								href=""
+								className="text-neutral-500 font-light hover:text-neutral-600"
+							>
+								Dropdown Link 3
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
