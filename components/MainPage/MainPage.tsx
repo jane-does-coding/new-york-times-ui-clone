234f6cee4463b183db33961a370dfe106055ee6a
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-08-27 09:34:14 -0600

    main page

--- components/MainPage/MainPage.tsx
+++ components/MainPage/MainPage.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const MainPage = () => {
-	return <div className="min-h-screen bg-neutral-200"></div>;
+	return (
+		<div className="min-h-screen bg-neutral-200 flex">
+			<div className="flex-3 w-2/3">
+				<div className="flex w-[90%] mx-auto gap-12">
+					<div className="flex flex-col w-2/5">
+						<h2 className="text-[1.15rem] font-bold text-neutral-800 mb-4">
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+							necessitatibus sed laudantium deserunt aperiam. Quis.
+						</h2>
+						<p className="text-neutral-500 text-[1rem] font-light">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+							quod odit distinctio illo repellat. Rem quis voluptatibus sequi
+							reprehenderit fuga.
+						</p>
+					</div>
+					<div className="bg-neutral-500 w-3/5 aspect-[3/2]"></div>
+				</div>
+			</div>
+			<div className="flex-1 w-1/3">
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas neque
+					nam blanditiis officia laudantium dignissimos dolorum. Culpa, ipsa
+					quae voluptatem illum cumque totam dolore, fugiat alias, cum rem
+					voluptate temporibus sequi aut autem minus? Quidem, at eaque. Dolor
+					saepe vel officiis vitae nostrum dicta qui a blanditiis itaque,
+					repellat aliquid minus molestiae libero accusamus autem deleniti, amet
+					incidunt, similique reiciendis eos quam quibusdam asperiores! Deleniti
+					tempore commodi numquam eveniet temporibus, ducimus repellendus quia
+					eum molestias dicta praesentium magnam debitis voluptate
+					necessitatibus soluta consequuntur quibusdam ad. Reiciendis est veniam
+					iste fuga repellendus eos tenetur, hic, modi magnam nesciunt, deleniti
+					rerum. Voluptas!
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default MainPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="flex flex-col bg-neutral-300 items-center justify-center py-8 z-[999]">
+		<div className="flex flex-col bg-neutral-300 items-center justify-center py-8 z-[999 pb-0">
 			<div className="flex text-neutral-800 font-light gap-8 text-[0.9rem]">
 				<a href="">Some Link</a>
 				<a href="">Some Link</a>
@@ -362,6 +362,20 @@ const Navbar = () => {
 						</div>
 					</div>
 				</div>
+			</div>
+			<div className="border-t-[2px] border-neutral-800 w-full mt-4 mb-[4px]"></div>
+			<div className="border-y-[2px] border-neutral-800 w-full flex items-center justify-center text-neutral-800 font-bold gap-6 py-2 text-[0.8rem]">
+				<a href="" className="text-red-600 font-bold">
+					LIVE
+				</a>
+				<a href="">
+					U.S. Presidential Race{" "}
+					<span className="font-light text-red-600 ml-[3px]">14 min ago</span>
+				</a>
+				<a href="">
+					U.S. Presidential Race{" "}
+					<span className="font-light text-red-600 ml-[3px]">14 min ago</span>
+				</a>
 			</div>
 		</div>
 	);
